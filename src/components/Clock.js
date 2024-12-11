0174fb891b2c54c6eac584fb9f01ef3e7ad5e0b6
AprilFoolsDay0401 <dkkim6@uci.edu>
2024-12-11 13:31:59 -0800

    UI change for Clock

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -6,18 +6,18 @@ const Clock = () => {
 
   useEffect(() => {
     const timer = setInterval(() => {
-      
-      setCurrentTime(dayjs().format("hh:mm:ss A"));
+      setCurrentTime(dayjs().format("h:mm:ss A"));
     }, 1000); // Update every one sec
 
     return () => clearInterval(timer); // clear timer when unmount
   }, []);
 
-  const [currentDate, setCurrentDate] = useState(dayjs().format("MMMM D, YYYY"));
+  const [currentDate, setCurrentDate] = useState(
+    dayjs().format("MMMM D, YYYY")
+  );
 
   useEffect(() => {
     const date = setInterval(() => {
-      
       setCurrentDate(dayjs().format("MMMM D, YYYY"));
     }, 1000); // Update every one sec
 
@@ -25,9 +25,9 @@ const Clock = () => {
   }, []);
 
   return (
-    <div className="text-4xl font-bold text-white">
-      <div>{currentTime}</div>
-      <div>{currentDate}</div>
+    <div className=" font-bold text-white">
+      <div className="text-xl">{currentDate}</div>
+      <div className="text-4xl">{currentTime}</div>
     </div>
   );
 };
