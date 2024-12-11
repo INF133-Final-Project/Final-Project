f93967c4895dba819cfce23772a1f1dc8185e5d9
Julie Cao <caojh1@uci.edu>
2024-12-11 05:52:07 -0800

    fix: fix spacing and UI

--- src/pages/Budget.js
+++ src/pages/Budget.js
@@ -103,102 +103,77 @@ const Budget = () => {
         className="flex flex-col items-center justify-center rounded-lg bg-gray-400 mx-3 mt-5 text-white px-4"
         style={{ height: "calc(100vh - 4.5rem)" }}
       >
-        {/* Weekly Budget Section */}
-        <div className="w-1/2 mb-6 text-center">
-          <div className="mb-4 space-y-2">
-            <div className="flex justify-between space-x-10">
-              {/* Current Budget */}
-               {/* Weekly Budget Section with Edit Icon */}
-          <div className="flex justify-between items-center">
-            <div className="w-1/2 text-left">
-              <p className="text-md font-medium text-gray-700 whitespace-nowrap">WEEKLY BUDGET</p>
-              <p className="font-bold text-4xl text-white">
-                ${weeklyBudget.toFixed(2)}
-              </p>
-            </div>
-            <button
-              onClick={toggleEditBudgetPopup}
-              className="relative text-green-600 text-xl bg-transparent border-0"
-              style={{ top: "18px", right: "15px" }}
-            >
-              <FaEdit />
-            </button>
-          </div>
+        {/* Budget Section */}
+        <div className="w-full h-full mt-10 text-center">
+          <div className="mb-2 space-y-2">
+            <div className="flex justify-between gap-x-4">
+              {/* Weekly Budget Section */}
+              <div className="flex items-center w-1/2 bg-transparent border-2 border-white p-4 rounded-lg">
+                <div className="w-full text-left">
+                  <p className="text-md font-medium text-gray-700 whitespace-nowrap">WEEKLY BUDGET</p>
+                  <div className="flex items-center">
+                    <p className="font-bold text-4xl text-white">${weeklyBudget.toFixed(2)}</p>
+                    <button
+                      onClick={toggleEditBudgetPopup}
+                      className="text-green-600 text-xl bg-transparent border-0 ml-2 mt-2"
+                    >
+                      <FaEdit />
+                    </button>
+                  </div>
+                </div>
+              </div>
 
-          {/* Modal for Editing Weekly Budget */}
-          {isEditBudgetPopupOpen && (
-            <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-              <div className="bg-white rounded-lg shadow-lg w-1/3 p-6">
-                <h2 className="text-2xl font-bold mb-4">Edit Weekly Budget</h2>
-                <div className="mb-4">
-                  <label className="block text-gray-700 font-bold mb-2">New Weekly Budget ($)</label>
-                  <input
-                    type="number"
-                    value={newWeeklyBudget}
-                    onChange={(e) => setNewWeeklyBudget(e.target.value)}
-                    className="w-full p-2 border rounded text-black"
-                  />
-                </div>
-                <div className="flex justify-end">
-                  <button
-                    onClick={toggleEditBudgetPopup}
-                    className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded mr-2"
-                  >
-                    Cancel
-                  </button>
-                  <button
-                    onClick={handleUpdateBudget}
-                    className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-                  >
-                    Update
-                  </button>
+              {/* Total Expenses Section */}
+              <div className="flex justify-between items-center w-1/2 bg-transparent border-2 border-white p-4 rounded-lg">
+                <div className="w-full text-left">
+                  <p className="text-md font-medium text-gray-700 whitespace-nowrap">TOTAL EXPENSES</p>
+                  <p className="font-bold text-4xl text-white">${totalExpenses.toFixed(2)}</p>
                 </div>
               </div>
             </div>
-          )}
-              {/* Total Expenses */}
-              <div className="w-1/2 text-left">
-                <p className="text-md font-medium text-gray-700 whitespace-nowrap">TOTAL EXPENSES</p>
-                <p className="font-bold text-4xl text-white">${totalExpenses.toFixed(2)}</p>
-              </div>
-            </div>
-             {/* Remaining Budget */}
-            {/* <div>
-              <p className="text-lg font-medium text-gray-700">REMAINING</p>
-              <p
-                className={`font-bold text-4xl ${
-                  remainingBudget < 0 ? "text-red-500" : "text-green-500"
-                }`}
-              >
-                ${remainingBudget.toFixed(2)}
-              </p>
-            </div> */}
           </div>
-          {/* <div className="flex justify-center items-center">
-            <input
-              type="number"
-              placeholder="Set Weekly Budget"
-              value={budgetInput}
-              onChange={(e) => setBudgetInput(e.target.value)}
-              className="p-2 border rounded mr-2 text-black"
-            />
-            <button
-              onClick={updateWeeklyBudget}
-              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-            >
-              Update
-            </button>
-          </div> */}
         </div>
 
+        {/* Modal for Editing Weekly Budget */}
+        {isEditBudgetPopupOpen && (
+          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+            <div className="bg-white rounded-lg shadow-lg w-1/3 p-6">
+              <h2 className="text-2xl font-bold mb-4">Edit Weekly Budget</h2>
+              <div className="mb-4">
+                <label className="block text-gray-700 font-bold mb-2">New Weekly Budget ($)</label>
+                <input
+                  type="number"
+                  value={newWeeklyBudget}
+                  onChange={(e) => setNewWeeklyBudget(e.target.value)}
+                  className="w-full p-2 border rounded text-black"
+                />
+              </div>
+              <div className="flex justify-end">
+                <button
+                  onClick={toggleEditBudgetPopup}
+                  className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded mr-2"
+                >
+                  Cancel
+                </button>
+                <button
+                  onClick={handleUpdateBudget}
+                  className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                >
+                  Update
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+
       {/* Chart Section with Toggle */}
-      <div className="w-1/2 mb-6">
+      <div className="w-1/2 mb-2">
         <div className="flex justify-center mb-4">
           <div className="flex items-center bg-gray-200 rounded-full p-1">
             {/* Pie Chart Button */}
             <button
               onClick={() => setChartType("pie")}
-              className={`px-4 py-2 rounded-full font-medium ${
+              className={`px-4 py-2 rounded-full font-medium text-xs ${
                 chartType === "pie" ? "bg-green-600 text-white" : "bg-transparent text-gray-500"
               }`}
             >
@@ -207,7 +182,7 @@ const Budget = () => {
             {/* Bar Chart Button */}
             <button
               onClick={() => setChartType("bar")}
-              className={`px-4 py-2 rounded-full font-medium ${
+              className={`px-4 py-2 rounded-full font-medium text-xs ${
                 chartType === "bar" ? "bg-green-600 text-white" : "bg-transparent text-gray-500"
               }`}
             >
@@ -219,33 +194,29 @@ const Budget = () => {
       </div>
 
         {/* Transactions List */}
-        <div className="w-1/2 mb-6">
-          {/* Total Expenses */}
-          {/* <div className="text-lg font-bold text-center mb-4">
-            Total Expenses: ${totalExpenses.toFixed(2)}
-          </div> */}
-          <ul className="bg-white text-black p-4 rounded-lg">
-            {transactions.length === 0 ? (
-              <p className="text-center">No transactions yet.</p>
-            ) : (
-              transactions.map((transaction, index) => (
-                <li
-                  key={index}
-                  className="flex justify-between items-center py-2 border-b last:border-b-0"
-                >
-                  <div>
-                    <p className="font-bold">{transaction.name}</p>
-                    <p className="text-sm text-gray-600">
-                      {transaction.date.toLocaleDateString()} 
-                    </p>
-                  </div>
-                  <span>${transaction.amount.toFixed(2)}</span>
-                  <span className="italic text-gray-600">{transaction.category}</span>
-                </li>
-              ))
-            )}
-          </ul>
-        </div>
+      <div className="w-full mt-8 mb-20">
+        <ul className="bg-white text-black p-4 rounded-lg">
+          {transactions.length === 0 ? (
+            <p className="text-center">No transactions yet.</p>
+          ) : (
+            transactions.map((transaction, index) => (
+              <li
+                key={index}
+                className="flex justify-between items-center py-2 border-b last:border-b-0"
+              >
+                <div className="mr-3">
+                  <p className="font-bold break-words">{transaction.name}</p>
+                  <p className="text-xs text-gray-600">
+                    {transaction.date.toLocaleDateString()} 
+                  </p>
+                </div>
+                <span className="italic text-gray-600 mr-3">{transaction.category}</span>
+                <span>- ${transaction.amount.toFixed(2)}</span>
+              </li>
+            ))
+          )}
+        </ul>
+      </div>
 
         {/* Add Transaction Button */}
         <button
