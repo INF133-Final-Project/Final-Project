93769c30b69a29a5ecfb83904a4c0ec593a0847d
AprilFoolsDay0401 <dkkim6@uci.edu>
2024-12-11 21:04:49 -0800

    Update the Note.js & NotesCreateEditModal.js(Jenny)

--- src/components/NotesCreateEditModal.js
+++ src/components/NotesCreateEditModal.js
@@ -1,114 +1,100 @@
 import React from "react";
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const NotesCreateEditModal = ({
-    isOpen,
-    isAnimating,
-    editIndex,
-    newNote,
-    setNewNote,
-    newTitle,
-    setNewTitle,
-    lastEdited,
-    setLastEdited,
-    tag,
-    setTag,
-    closeModal,
-    addOrEditNote,
-    deleteNote,
+  isOpen,
+  isAnimating,
+  editIndex,
+  newNote,
+  setNewNote,
+  newTitle,
+  setNewTitle,
+  lastEdited,
+  setLastEdited,
+  tag,
+  setTag,
+  closeModal,
+  addOrEditNote,
+  deleteNote,
 }) => {
-    if(!isOpen) return null;
+  if (!isOpen) return null;
 
-    return (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 transition-opacity duration-700">
-          <div
-            className={`bg-gray-300 p-6 rounded-lg shadow-lg sm:w-1/2 w-3/4 max-w-2xl sm:mx-0 mx-4 text-center transform transition-all duration-700 ${
-              isAnimating
-                ? "opacity-100 translate-y-0"
-                : "opacity-0 translate-y-full"
-            }`}
-          >
-            <h2 className="text-2xl font-semibold mb-4">
-              {editIndex !== null ? "Edit Note" : "Add New Note"}
-            </h2>
-            <input
-              type="text"
-              value={newTitle}
-              onChange={(e) => setNewTitle(e.target.value)}
-              placeholder="Enter Title"
-              className="w-full p-2 mb-4 rounded bg-gray-200 text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-orange-400"
-            />
-            <ReactQuill
-              value={newNote}
-              onChange={setNewNote}
-              modules={{
-                toolbar: [
-                  [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
-                  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-                  ['bold', 'italic', 'underline'],
-                  [{ 'align': [] }],
-                  ['link'],
-                ],
-              }}
-              className="mb-4"
-            />
-            
-            <textarea
-              type="text"
-              value={newNote}
-              onChange={(e) => setNewNote(e.target.value)}
-              rows = "10"
-              placeholder="Enter Note"
-              className="w-full p-4 mb-4 rounded bg-gray-200 text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-orange-400"
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 transition-opacity duration-700">
+      <div
+        className={`bg-gray-300 p-6 rounded-lg shadow-lg sm:w-1/2 w-3/4 max-w-2xl sm:mx-0 mx-4 text-center transform transition-all duration-700 ${
+          isAnimating
+            ? "opacity-100 translate-y-0"
+            : "opacity-0 translate-y-full"
+        }`}
+      >
+        <h2 className="text-2xl font-semibold mb-4">
+          {editIndex !== null ? "Edit Note" : "Add New Note"}
+        </h2>
+        <input
+          type="text"
+          value={newTitle}
+          onChange={(e) => setNewTitle(e.target.value)}
+          placeholder="Enter Title"
+          className="w-full p-2 mb-4 rounded bg-gray-200 text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-orange-400"
+        />
+        <ReactQuill
+          type="text"
+          value={newNote}
+          onChange={setNewNote}
+          rows="10"
+          placeholder="Enter Note"
+          modules={{
+            toolbar: [
+              [{ header: "1" }, { header: "2" }, { font: [] }],
+              [{ list: "ordered" }, { list: "bullet" }],
+              ["bold", "italic", "underline"],
+              [{ align: [] }],
+              ["link"],
+            ],
+          }}
+          className="w-full p-4 mb-4 rounded bg-gray-200 text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        />
+        <h3 className="text-1xl mb-2">Select a Tag</h3>
+        <select
+          value={tag}
+          onChange={(e) => setTag(e.target.value)}
+          className="w-full p-2 mb-4 rounded bg-gray-200 text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        >
+          <option value="Empty"> </option>
+          <option value="Tag1">Tag1</option>
+          <option value="Tag2">Tag2</option>
+          <option value="Tag3">Tag3</option>
+        </select>
 
-            />
-            
-            <input
-              type="datetime-local"
-              value={lastEdited}
-              onChange={(e) => setLastEdited(e.target.value)}
-              className="w-full p-2 mb-4 rounded bg-gray-200 text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-orange-400"
-            />
-            
-            <select
-              value={tag}
-              onChange={(e) => setTag(e.target.value)}
-              className="w-full p-2 mb-4 rounded bg-gray-200 text-gray-900 focus:outline-none focus:ring-2 focus:ring-orange-400"
+        <div className="flex justify-end items-center space-x-2">
+          {editIndex !== null && (
+            <button
+              onClick={deleteNote}
+              className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-auto"
             >
-
-
-              <option value="Tag1">Tag1</option>
-              <option value="Tag2">Tag2</option>
-              <option value="Tag3">Tag3</option>
-            </select>
-            <div className="flex justify-end items-center space-x-2">
-              {editIndex !== null && (
-                <button
-                  onClick={deleteNote}
-                  className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-auto"
-                >
-                  Delete
-                </button>
-              )}
-              <div className="flex space-x-2">
-                <button
-                  onClick={closeModal}
-                  className="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded"
-                >
-                  Cancel
-                </button>
-                <button
-                  onClick={addOrEditNote}
-                  className="bg-orange-400 hover:bg-orange-500 text-white font-bold py-2 px-4 rounded"
-                >
-                  Confirm
-                </button>
-              </div>
-            </div>
+              Delete
+            </button>
+          )}
+          <div className="flex space-x-2">
+            <button
+              onClick={closeModal}
+              className="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded"
+            >
+              Cancel
+            </button>
+            <button
+              onClick={addOrEditNote}
+              className="bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded"
+            >
+              Confirm
+            </button>
           </div>
-        </div> 
-    );
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NotesCreateEditModal;
--- src/pages/Note.js
+++ src/pages/Note.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import 'react-quill/dist/quill.snow.css';
+import "react-quill/dist/quill.snow.css";
+import DOMPurify from "dompurify";
 import { db, auth } from "../firebaseConfig";
 import ErrorModal from "../components/ErrorModal";
 import {
@@ -15,14 +16,17 @@ import {
 import { useAuthState } from "react-firebase-hooks/auth";
 import { PlusIcon } from "@heroicons/react/24/outline";
 import NotesCreateEditModal from "../components/NotesCreateEditModal";
-
+import { Input } from "postcss";
+import { BsSearch } from "react-icons/bs";
 
 const Note = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [newTitle, setNewTitle] = useState("");
   const [lastEdited, setLastEdited] = useState("");
-  const [tag, setTag] = useState("Tag1");
+  const [tag, setTag] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredNotes, setFilteredNotes] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isAnimating, setIsAnimating] = useState(false);
   const [editIndex, setEditIndex] = useState(null);
@@ -30,19 +34,23 @@ const Note = () => {
   const [errorModal, setErrorModal] = useState({ isOpen: false, message: "" });
 
   const [user] = useAuthState(auth);
-  
+
   const openModal = (index = null) => {
+    // Opening a previously existing note
     if (index !== null) {
       const note = notes[index];
       setNewTitle(note.title);
-      setNewNote(note.noteText);
-      setLastEdited(note.edit);
+      setNewNote(note.text || "");
+      setLastEdited(note.edit || Date.now());
       setTag(note.tag);
       setEditIndex(index);
-    } else {
+    }
+
+    // Creating a new note
+    else {
       setNewTitle("");
       setNewNote("");
-      setLastEdited("");
+      setLastEdited(Date.now());
       setTag("");
       setEditIndex(null);
     }
@@ -50,19 +58,23 @@ const Note = () => {
     setTimeout(() => setIsAnimating(true), 0);
   };
 
+  // closing the right side of the page
   const closeModal = () => {
     setIsAnimating(false);
     setTimeout(() => setIsModalOpen(false), 700);
   };
-
+  // closing the error modal
   const closeErrorModal = () => {
     setErrorModal(false);
   };
 
+  // creating or editing an existing note
   const addOrEditNote = async () => {
-    if(newTitle.trim() && newNote.trim() && lastEdited && user) {
+    // if the user has filled in all fields
+    if (newTitle.trim() && newNote.trim() && lastEdited && user) {
+      // grab the time the user pressed "Confirm"
       const currentStamp = Date.now();
-
+      //update the title, text, last edited, and tag information
       const noteData = {
         title: newTitle,
         text: newNote,
@@ -70,7 +82,7 @@ const Note = () => {
         tag,
       };
 
-      if(editIndex !== null) {
+      if (editIndex !== null) {
         const noteRef = doc(
           db,
           "users",
@@ -83,12 +95,12 @@ const Note = () => {
         await addDoc(collection(db, "users", user.uid, "notes"), noteData);
       }
       closeModal();
-      
     } else {
-      setErrorModal({ isOpen: true, message: "Please fill in all fields."});
+      setErrorModal({ isOpen: true, message: "Please fill in all fields." });
     }
   };
 
+  // deleting a note
   const deleteNote = async () => {
     if (editIndex !== null && user) {
       const noteRef = doc(db, "users", user.uid, "notes", notes[editIndex].id);
@@ -98,6 +110,19 @@ const Note = () => {
     }
   };
 
+  // function that filters through the notes by title
+  function handleSearch() {
+    if (searchQuery === "") {
+      setFilteredNotes(notes);
+    } else {
+      const filterBySearch = notes.filter((note) =>
+        note.title.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+      setFilteredNotes(filterBySearch);
+    }
+  }
+
+  // sets the border of the note to the color of the tag
   const getBorderColor = (tag) => {
     switch (tag) {
       case "Tag1":
@@ -111,6 +136,7 @@ const Note = () => {
     }
   };
 
+  // sets the tag to the color corresponding to it
   const getFontColor = (tag) => {
     switch (tag) {
       case "Tag1":
@@ -162,7 +188,60 @@ const Note = () => {
       className="flex flex-col items-center justify-center rounded-lg bg-gray-400 mx-3 mt-5 text-gray-900 px-4 "
       style={{ height: "calc(100vh - 4.5rem)" }}
     >
-      <h1 className="text-4xl font-black mt-10 mb-5 text-white">Notes List</h1>
+      <h1 className="text-4xl font-black mt-10 mb-5 text-white"></h1>
+      <div className="w-full max-w-2xl mb-5 h-auto y-auto">
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <input
+            className="w-full p-2 mb-4 rounded bg-gray-200 text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400"
+            onChange={(e) => setSearchQuery(e.target.value)}
+            placeholder="Search notes"
+            style={{ flex: 1, marginRight: "8px" }}
+          ></input>
+          <BsSearch
+            onClick={handleSearch}
+            style={{ cursor: "pointer", marginRight: "8px" }}
+          />
+        </div>
+        <div>
+          {filteredNotes.map((note) => {
+            const originalIndex = notes.findIndex((n) => n.id === note.id);
+            const newDate = new Date(note.edit).toLocaleString("en-US", {
+              year: "numeric",
+              month: "short",
+              day: "numeric",
+              hour: "2-digit",
+              minute: "2-digit",
+            });
+
+            return (
+              <li
+                key={note.id}
+                onClick={() => openModal(originalIndex)}
+                className={`w-full max-w-2xl mb-5 bg-gray-100 p-3 rounded-md shadow-md flex flex-col border-r-8 ${getBorderColor(
+                  note.tag
+                )} cursor-pointer hover:bg-gray-300 transition duration-200`}
+              >
+                <div className="flex items-center">
+                  <div className="flex flex-col ml-2 flex-grow">
+                    <span style={{ fontWeight: "bold" }}>{note.title}</span>
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: DOMPurify.sanitize(note.text),
+                      }}
+                    />
+                    <p className="text-xs text-gray-500 mt-1">
+                      {newDate} |{" "}
+                      <span className={`font-bold ${getFontColor(note.tag)}`}>
+                        {note.tag}
+                      </span>
+                    </p>
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+        </div>
+      </div>
 
       <div className="w-full max-w-2xl mb-5 h-full overflow-y-auto">
         {notes.length > 0 ? (
@@ -172,39 +251,37 @@ const Note = () => {
                 year: "numeric",
                 month: "short",
                 day: "numeric",
+                hour: "2-digit",
+                minute: "2-digit",
               });
 
               return (
                 <li
-                key={index}
-                onClick={() => openModal(index)}
-                className={`bg-gray-100 p-3 rounded-md shadow-md flex flex-col border-r-8 ${getBorderColor(
-                  note.tag
-                )} cursor-pointer hover:bg-gray-300 transition duration-200`}
+                  key={index}
+                  onClick={() => openModal(index)}
+                  className={`bg-gray-100 p-3 rounded-md shadow-md flex flex-col border-r-8 ${getBorderColor(
+                    note.tag
+                  )} cursor-pointer hover:bg-gray-300 transition duration-200`}
                 >
-                <div className="flex items-center">
+                  <div className="flex items-center">
                     <div className="flex flex-col ml-2 flex-grow">
-                      <span style={{fontWeight: 'bold'}}>
-                        {note.title}
-                      </span>
-                      <span>
-                        {note.text}
-                      </span>
+                      <span style={{ fontWeight: "bold" }}>{note.title}</span>
+                      <span
+                        dangerouslySetInnerHTML={{
+                          __html: DOMPurify.sanitize(note.text),
+                        }}
+                      />
                       <p className="text-xs text-gray-500 mt-1 ">
-                        {lastEdited}/{" "}
+                        {lastEdited} |{" "}
                         <span
-                          className={`font-bold ${getFontColor(
-                            note.tag
-                          )} `}
+                          className={`font-bold ${getFontColor(note.tag)} `}
                         >
                           {note.tag}
                         </span>
                       </p>
                     </div>
                   </div>
-                  </li>
-
-
+                </li>
               );
             })}
           </ul>
@@ -213,14 +290,15 @@ const Note = () => {
             No notes yet!!
           </p>
         )}
-
       </div>
 
       <button
         onClick={() => openModal()}
-        className="fixed bottom-8 right-8 bg-orange-400 hover:bg-orange-500 text-white font-bold py-3 px-5 rounded-full shadow-lg transition duration-300"
+        className="fixed bottom-8 right-8 bg-blue-400 hover:bg-blue-500 text-white font-bold py-3 px-5 rounded-full shadow-lg transition duration-300"
       >
-        <span className="inline sm:hidden">+</span>
+        <span className="inline sm:hidden">
+          <PlusIcon className="w-6 h-6" strokeWidth={3} />
+        </span>
         <span className="hidden sm:inline">+ Create</span>
       </button>
 
@@ -248,7 +326,6 @@ const Note = () => {
         isError={true}
       />
     </div>
-
   );
 };
 
